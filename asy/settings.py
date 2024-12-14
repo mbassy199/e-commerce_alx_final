6d469fad5a1974f20714a663c4bd5f0250600508
mbassy199 <awuleynovember@gmail.com>
2024-12-14 13:10:37 +0000

    update

--- asy/settings.py
+++ asy/settings.py
@@ -14,9 +14,6 @@ import os
 from pathlib import Path
 from decouple import config
 import dj_database_url
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
