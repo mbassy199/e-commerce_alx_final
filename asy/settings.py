ff2308f27e2462e514f579fe6d40786a2ac13cab
mbassy199 <awuleynovember@gmail.com>
2024-12-14 12:38:28 +0000

    update

--- asy/settings.py
+++ asy/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOTCLEAR = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'asy/static',  # Updated project name
 ]
