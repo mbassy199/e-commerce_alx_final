e2d48cac59f395018e86e7748420ed5726cfb61f
mbassy199 <awuleynovember@gmail.com>
2024-12-15 01:43:52 +0000

    update

--- asy/settings.py
+++ asy/settings.py
@@ -37,7 +37,7 @@ COMPRESS_ROOT = BASE_DIR / 'static'
 
 COMPRESS_ENABLED = True
 
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+#STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 # Application definition
 INSTALLED_APPS = [
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOTCLEAR = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'asy/static',  # Updated project name
 ]
